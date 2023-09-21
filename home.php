94283503c23ec55501b715cfb98414c12dca8cd4
ananya2118 <107682269+ananya2118@users.noreply.github.com>
2023-09-21 22:40:55 +0530

    Update home.php

--- home.php
+++ home.php
@@ -16,18 +16,31 @@
 
     <div class="department cardiology">
     <h3><a href="cardiology.php" class="anch">Cardiology</a></h3>
-        <p>Our Cardiology Department specializes in heart-related conditions and treatments.</p>
+    <p>The Cardiology Department at Ramaiah Hospital is dedicated to providing comprehensive care for heart-related conditions.
+         Our team of experienced cardiologists and state-of-the-art facilities ensure that patients receive the best possible treatment 
+         and care.
+        Services and treatments offered by our Cardiology Department include:
+        
+            Cardiac consultations and diagnostics.
+            Coronary artery disease management.
+           Cardiac surgeries and interventions.
+            Electrocardiography (ECG) and echocardiography.
+            
+      
+        Whether you're seeking preventive care or treatment for a heart condition, our Cardiology Department is here to provide you with expert care and support.</p>
     </div>
     <div class="department neurology">
-        <h3>Neurology</h3>
-        <p class=neur>The Orthopedics Department focuses on bone and joint health, offering orthopedic surgeries and treatments.</p>
+        <h3><a href="neurology.php" class="anch">Neurology</a></h3>
+        <p>The Neurology Department at Ramaiah Hospital specializes in diagnosing and treating various neurological conditions affecting the brain, spinal cord, and nervous system. Our team of highly skilled neurologists is dedicated to providing the best possible care to our patients.
+        We offer comprehensive care for conditions such as stroke management, epilepsy diagnosis and treatment, neuromuscular disorders, headaches, migraines, memory and cognitive disorders, and more.
+        With state-of-the-art diagnostic and therapeutic technologies, our department ensures accurate diagnosis and effective treatment. We prioritize compassionate, patient-centered care, supporting individuals on their journey to better neurological health.</p>
     </div>
     <div class="department orthopedics">
-        <h3>Orthopedics</h3>
+        <h3><a href="orthopaedics.php" class="anch">Orthopedics</a></h3>
         <p>The Orthopedics Department focuses on bone and joint health, offering orthopedic surgeries and treatments.</p>
     </div>
     <div class="department obgyn">
-        <h3>Obstetrics & Gynecology</h3>
+        <h3><a href="obgyn.php" class="anch">Obstetrics & Gynecology</a></h3>
         <p>Our Obstetrics & Gynecology Department provides care for women's health, including pregnancy and childbirth.</p>
     </div>
     <!-- Add more departments as needed -->
