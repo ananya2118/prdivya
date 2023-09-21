707e489c0a759f171a5c01503f4cc3425623c3a5
ananya2118 <107682269+ananya2118@users.noreply.github.com>
2023-09-21 22:12:58 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hospital Management</title>
+    <link rel="stylesheet" href="style1.css">
+</head>
+<body>
+    <header>
+        <h1>Departments</h1>
+
+    </header>
+
+    <section class="departments">
+
+    <div class="department cardiology">
+    <h3><a href="cardiology.php" class="anch">Cardiology</a></h3>
+        <p>Our Cardiology Department specializes in heart-related conditions and treatments.</p>
+    </div>
+    <div class="department neurology">
+        <h3>Neurology</h3>
+        <p class=neur>The Orthopedics Department focuses on bone and joint health, offering orthopedic surgeries and treatments.</p>
+    </div>
+    <div class="department orthopedics">
+        <h3>Orthopedics</h3>
+        <p>The Orthopedics Department focuses on bone and joint health, offering orthopedic surgeries and treatments.</p>
+    </div>
+    <div class="department obgyn">
+        <h3>Obstetrics & Gynecology</h3>
+        <p>Our Obstetrics & Gynecology Department provides care for women's health, including pregnancy and childbirth.</p>
+    </div>
+    <!-- Add more departments as needed -->
+</section>
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,65 @@
+<?php 
+session_start();
+
+include("php/config.php");
+
+if (isset($_POST['submit'])) {
+    $email = mysqli_real_escape_string($con, $_POST['email']);
+    $password = mysqli_real_escape_string($con, $_POST['password']);
+    $result = mysqli_query($con, "SELECT * FROM users WHERE Email='$email' AND Password='$password' ") or die("Select Error");
+    $row = mysqli_fetch_assoc($result);
+
+    if (is_array($row) && !empty($row)) {
+        $_SESSION['valid'] = $row['Email'];
+        $_SESSION['username'] = $row['Username'];
+        $_SESSION['age'] = $row['Age'];
+        $_SESSION['id'] = $row['Id'];
+        header("Location: home.php");
+    } else {
+        $errorMessage = "Wrong Username or Password";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Login</title>
+</head>
+<body>
+    <div class="container">
+        <div class="box form-box">
+            <header>Login</header>
+            <form action="" method="post">
+                <div class="field input">
+                    <label for="email">Email</label>
+                    <input type="text" name="email" id="email" autocomplete="off" required>
+                </div>
+
+                <div class="field input">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password" autocomplete="off" required>
+                </div>
+
+                <div class="field">
+                    <input type="submit" class="btn" name="submit" value="Login" required>
+                </div>
+                
+                <?php if (isset($errorMessage)) : ?>
+                    <div class="message">
+                        <p><?= $errorMessage ?></p>
+                    </div>
+                <?php endif; ?>
+
+                <div class="links">
+                    Don't have an account? <a href="register.php" class="anch">Sign Up Now</a>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
